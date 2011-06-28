343108f3e209d01ffe79b8413758b060fd2bba32
Sebastian Muggelberg <mail@muggelberg.com>
2011-06-28 10:59:34 +0200

    Initial Commit

---
+++ DailyDealTile/DailyDealTileAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  DailyDealTileAppDelegate.h
+//  DailyDealTile
+//
+//  Created by Sebastian Muggelberg on 28.06.11.
+//  Copyright 2011 HTW Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DailyDealTileViewController;
+
+@interface DailyDealTileAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet DailyDealTileViewController *viewController;
+
+@end
---
+++ DailyDealTile/DailyDealTileViewController.h
@@ -0,0 +1,15 @@
+//
+//  DailyDealTileViewController.h
+//  DailyDealTile
+//
+//  Created by Sebastian Muggelberg on 28.06.11.
+//  Copyright 2011 HTW Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyDealTileViewController : UIViewController {
+    
+}
+
+@end
